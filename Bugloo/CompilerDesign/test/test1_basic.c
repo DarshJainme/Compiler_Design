4e270203492b2f609768389a74751deb39213662
lavanyasinghal28 <lavanya_s@cs.iitr.ac.in>
2025-08-10 21:23:10 +0530

    Update test1_basic.c

--- Bugloo/CompilerDesign/test/test1_basic.c
+++ Bugloo/CompilerDesign/test/test1_basic.c
@@ -2,7 +2,7 @@ int main() {
     int x = 10;
     float y = 3.14;
     char c = 'A';
-    int 1a = 5; // It will not throw error but the parts should be identified correctly for the parser to throw syntax error
+    int 1a = 5; // basic testing
     c=(float)(10)+1;
     float z = 3.16e20;
     return 0;
