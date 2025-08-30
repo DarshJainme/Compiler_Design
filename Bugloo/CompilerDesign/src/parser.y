e9dbeaec750ecbf60497b7f04d7fc338052382f4
DarshJainme <darsh_j@cs.iitr.ac.in>
2025-08-30 09:16:18 +0530

    Removed invalid_number as it already came in INVALID_IDENTIFIER_START, changed tokens.h, and parser.y

--- Bugloo/CompilerDesign/src/parser.y
+++ Bugloo/CompilerDesign/src/parser.y
@@ -5,17 +5,37 @@
 
     extern int yylex();
     extern int yylineno;
+    extern FILE *yyin;
     void yyerror(const char *s);
 %}
 
-/* Declare tokens from lexer */
-%token IDENTIFIER NUMBER
-%token OP_LESS_THAN OP_GREATER_THAN COMMA
-%token KEYWORD_TYPEDEF KEYWORD_CLASS KEYWORD_ENUM KEYWORD_NAMESPACE
+/* Tell bison to generate the header (use -d flag when running bison). */
 
-/* You can also alias tokens if lexer and grammar names differ */
-%token OP_LESS_THAN OP_GREATER_THAN
-%left OP_LESS_THAN OP_GREATER_THAN
+%token KEYWORD_INT KEYWORD_CHAR KEYWORD_FLOAT KEYWORD_DOUBLE KEYWORD_VOID
+%token KEYWORD_IF KEYWORD_ELSE KEYWORD_FOR KEYWORD_WHILE KEYWORD_RETURN
+%token KEYWORD_BREAK KEYWORD_CONTINUE KEYWORD_DO KEYWORD_SWITCH KEYWORD_CASE
+%token KEYWORD_DEFAULT KEYWORD_GOTO KEYWORD_ENUM KEYWORD_STRUCT KEYWORD_UNION
+%token KEYWORD_TYPEDEF KEYWORD_STATIC KEYWORD_AUTO KEYWORD_CONST KEYWORD_SIZEOF
+%token KEYWORD_CLASS KEYWORD_PUBLIC KEYWORD_PRIVATE KEYWORD_PROTECTED
+%token KEYWORD_NEW KEYWORD_DELETE KEYWORD_USING KEYWORD_NAMESPACE KEYWORD_BOOL
+%token KEYWORD_LONG KEYWORD_TRUE KEYWORD_FALSE
+
+%token IDENTIFIER INTEGER_LITERAL FLOAT_LITERAL CHAR_LITERAL STRING_LITERAL
+
+%token OP_PLUS OP_MINUS OP_MULTIPLY OP_DIVIDE OP_MODULO
+%token OP_INCREMENT OP_DECREMENT OP_ASSIGN OP_ADD_ASSIGN OP_SUB_ASSIGN
+%token OP_MUL_ASSIGN OP_DIV_ASSIGN OP_MOD_ASSIGN OP_LSHIFT OP_RSHIFT
+%token OP_EQUAL OP_NOT_EQUAL OP_LESS_THAN OP_GREATER_THAN
+%token OP_LESS_THAN_EQUAL OP_GREATER_THAN_EQUAL OP_LOGICAL_AND OP_LOGICAL_OR
+%token OP_LOGICAL_NOT OP_BITWISE_AND OP_BITWISE_OR OP_BITWISE_XOR OP_BITWISE_NOT
+%token OP_AND OP_OR OP_XOR OP_NOT  /* in case lexer uses these synonyms */
+
+%token SEMICOLON COMMA OPEN_PARENTHESIS CLOSE_PARENTHESIS
+%token OPEN_BRACKET CLOSE_BRACKET OPEN_CURLY_BRACKET CLOSE_CURLY_BRACKET
+%token COLON POINTER_ARROW DOT REFERENCE_AMPERSAND POINTER_STAR QUESTION_MARK
+
+%token PREPROCESSOR_DIRECTIVE SINGLE_LINE_COMMENT MULTI_LINE_COMMENT
+%token NEWLINE WHITESPACE ERROR_TOKEN UNKNOWN END_OF_FILE
 
 %%
 
@@ -29,7 +49,6 @@ declaration_list
     | declaration
     ;
 
-/* Very simplified example declarations */
 declaration
     : typedef_declaration
     | class_declaration
@@ -37,18 +56,16 @@ declaration
     ;
 
 typedef_declaration
-    : KEYWORD_TYPEDEF typedef_name ';'
+    : KEYWORD_TYPEDEF typedef_name SEMICOLON
     ;
 
 class_declaration
-    : KEYWORD_CLASS class_name '{' '}'
+    : KEYWORD_CLASS class_name OPEN_CURLY_BRACKET CLOSE_CURLY_BRACKET
     ;
 
 enum_declaration
-    : KEYWORD_ENUM enum_name '{' '}'
+        : KEYWORD_ENUM enum_name OPEN_CURLY_BRACKET CLOSE_CURLY_BRACKET
     ;
-
-/* === NAMES === */
 typedef_name
     : IDENTIFIER
     | template_id
@@ -63,7 +80,6 @@ enum_name
     : IDENTIFIER
     ;
 
-/* === TEMPLATE === */
 template_id
     : IDENTIFIER OP_LESS_THAN template_argument_list OP_GREATER_THAN
     ;
@@ -75,7 +91,7 @@ template_argument_list
 
 template_argument
     : IDENTIFIER
-    | NUMBER
+    | INTEGER_LITERAL
     | template_id
     ;
 
@@ -84,4 +100,20 @@ template_argument
 /* Error handler */
 void yyerror(const char *s) {
     fprintf(stderr, "Parse Error at line %d: %s\n", yylineno, s);
+}
+
+/* simple driver */
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        yyin = fopen(argv[1], "r");
+        if (!yyin) {
+            perror("fopen");
+            return 1;
+        }
+    } else {
+        yyin = stdin;
+    }
+    yyparse();
+    if (yyin && yyin != stdin) fclose(yyin);
+    return 0;
 }
--- Bugloo/CompilerDesign/src/tokens.h
+++ Bugloo/CompilerDesign/src/tokens.h
@@ -1,225 +1,127 @@
 #ifndef TOKENS_H
 #define TOKENS_H
+#include "y.tab.h"
 
-enum TokenType
-{
-    // Keywords  
-    KEYWORD_INT = 1,
-    KEYWORD_CHAR,
-    KEYWORD_FLOAT,
-    KEYWORD_DOUBLE,
-    KEYWORD_VOID,
-    KEYWORD_IF,
-    KEYWORD_ELSE,
-    KEYWORD_FOR,
-    KEYWORD_WHILE,
-    KEYWORD_RETURN,
-    KEYWORD_BREAK,
-    KEYWORD_CONTINUE,
-    KEYWORD_DO,
-    KEYWORD_SWITCH,
-    KEYWORD_CASE,
-    KEYWORD_DEFAULT,
-    KEYWORD_GOTO,
-    KEYWORD_ENUM,
-    KEYWORD_STRUCT,
-    KEYWORD_UNION,
-    KEYWORD_TYPEDEF,
-    KEYWORD_STATIC,
-    KEYWORD_AUTO,
-    KEYWORD_CONST,
-    KEYWORD_SIZEOF,
-    KEYWORD_CLASS,
-    KEYWORD_PUBLIC,
-    KEYWORD_PRIVATE,
-    KEYWORD_PROTECTED,
-    KEYWORD_NEW,
-    KEYWORD_DELETE,
-    KEYWORD_USING,
-    KEYWORD_NAMESPACE,
-    KEYWORD_BOOL, 
-    KEYWORD_LONG,
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* Provide a helper to convert token values to readable strings.
+   We use a switch over token symbols (names) so we don't rely on numeric order. */
+static inline const char* token_to_string(int token) {
+    switch(token) {
+        /* Keywords */
+        case KEYWORD_INT:            return "KEYWORD_INT";
+        case KEYWORD_CHAR:           return "KEYWORD_CHAR";
+        case KEYWORD_FLOAT:          return "KEYWORD_FLOAT";
+        case KEYWORD_DOUBLE:         return "KEYWORD_DOUBLE";
+        case KEYWORD_VOID:           return "KEYWORD_VOID";
+        case KEYWORD_IF:             return "KEYWORD_IF";
+        case KEYWORD_ELSE:           return "KEYWORD_ELSE";
+        case KEYWORD_FOR:            return "KEYWORD_FOR";
+        case KEYWORD_WHILE:          return "KEYWORD_WHILE";
+        case KEYWORD_RETURN:         return "KEYWORD_RETURN";
+        case KEYWORD_BREAK:          return "KEYWORD_BREAK";
+        case KEYWORD_CONTINUE:       return "KEYWORD_CONTINUE";
+        case KEYWORD_DO:             return "KEYWORD_DO";
+        case KEYWORD_SWITCH:         return "KEYWORD_SWITCH";
+        case KEYWORD_CASE:           return "KEYWORD_CASE";
+        case KEYWORD_DEFAULT:        return "KEYWORD_DEFAULT";
+        case KEYWORD_GOTO:           return "KEYWORD_GOTO";
+        case KEYWORD_ENUM:           return "KEYWORD_ENUM";
+        case KEYWORD_STRUCT:         return "KEYWORD_STRUCT";
+        case KEYWORD_UNION:          return "KEYWORD_UNION";
+        case KEYWORD_TYPEDEF:        return "KEYWORD_TYPEDEF";
+        case KEYWORD_STATIC:         return "KEYWORD_STATIC";
+        case KEYWORD_AUTO:           return "KEYWORD_AUTO";
+        case KEYWORD_CONST:          return "KEYWORD_CONST";
+        case KEYWORD_SIZEOF:         return "KEYWORD_SIZEOF";
+        case KEYWORD_CLASS:          return "KEYWORD_CLASS";
+        case KEYWORD_PUBLIC:         return "KEYWORD_PUBLIC";
+        case KEYWORD_PRIVATE:        return "KEYWORD_PRIVATE";
+        case KEYWORD_PROTECTED:      return "KEYWORD_PROTECTED";
+        case KEYWORD_NEW:            return "KEYWORD_NEW";
+        case KEYWORD_DELETE:         return "KEYWORD_DELETE";
+        case KEYWORD_USING:          return "KEYWORD_USING";
+        case KEYWORD_NAMESPACE:      return "KEYWORD_NAMESPACE";
+        case KEYWORD_BOOL:           return "KEYWORD_BOOL";
+        case KEYWORD_LONG:           return "KEYWORD_LONG";
+        case KEYWORD_TRUE:           return "KEYWORD_TRUE";
+        case KEYWORD_FALSE:          return "KEYWORD_FALSE";
 
-    // Literals treated as keywords
-    KEYWORD_TRUE,
-    KEYWORD_FALSE,
+        /* Identifiers & literals */
+        case IDENTIFIER:            return "IDENTIFIER";
+        case INTEGER_LITERAL:       return "INTEGER_LITERAL";
+        case FLOAT_LITERAL:         return "FLOAT_LITERAL";
+        case CHAR_LITERAL:          return "CHAR_LITERAL";
+        case STRING_LITERAL:        return "STRING_LITERAL";
 
-    // Identifiers and literals
-    IDENTIFIER,
-    INTEGER_LITERAL,
-    FLOAT_LITERAL,
-    CHAR_LITERAL,
-    STRING_LITERAL,
+        /* Operators */
+        case OP_PLUS:               return "OP_PLUS";
+        case OP_MINUS:              return "OP_MINUS";
+        case OP_MULTIPLY:           return "OP_MULTIPLY";
+        case OP_DIVIDE:             return "OP_DIVIDE";
+        case OP_MODULO:             return "OP_MODULO";
+        case OP_INCREMENT:          return "OP_INCREMENT";
+        case OP_DECREMENT:          return "OP_DECREMENT";
+        case OP_ASSIGN:             return "OP_ASSIGN";
+        case OP_ADD_ASSIGN:         return "OP_ADD_ASSIGN";
+        case OP_SUB_ASSIGN:         return "OP_SUB_ASSIGN";
+        case OP_MUL_ASSIGN:         return "OP_MUL_ASSIGN";
+        case OP_DIV_ASSIGN:         return "OP_DIV_ASSIGN";
+        case OP_MOD_ASSIGN:         return "OP_MOD_ASSIGN";
+        case OP_LSHIFT:             return "OP_LSHIFT";
+        case OP_RSHIFT:             return "OP_RSHIFT";
+        case OP_AND:                return "OP_AND";
+        case OP_OR:                 return "OP_OR";
+        case OP_XOR:                return "OP_XOR";
+        case OP_NOT:                return "OP_NOT";
+        case OP_EQUAL:              return "OP_EQUAL";
+        case OP_NOT_EQUAL:          return "OP_NOT_EQUAL";
+        case OP_LESS_THAN:          return "OP_LESS_THAN";
+        case OP_GREATER_THAN:       return "OP_GREATER_THAN";
+        case OP_LESS_THAN_EQUAL:    return "OP_LESS_THAN_EQUAL";
+        case OP_GREATER_THAN_EQUAL: return "OP_GREATER_THAN_EQUAL";
+        case OP_LOGICAL_AND:        return "OP_LOGICAL_AND";
+        case OP_LOGICAL_OR:         return "OP_LOGICAL_OR";
+        case OP_LOGICAL_NOT:        return "OP_LOGICAL_NOT";
+        case OP_BITWISE_AND:        return "OP_BITWISE_AND";
+        case OP_BITWISE_OR:         return "OP_BITWISE_OR";
+        case OP_BITWISE_XOR:        return "OP_BITWISE_XOR";
+        case OP_BITWISE_NOT:        return "OP_BITWISE_NOT";
 
-    // Operators
-    OP_PLUS,
-    OP_MINUS,
-    OP_MULTIPLY,
-    OP_DIVIDE,
-    OP_MODULO,
-    OP_INCREMENT,
-    OP_DECREMENT,
-    OP_ASSIGN,
-    OP_ADD_ASSIGN,
-    OP_SUB_ASSIGN,
-    OP_MUL_ASSIGN,
-    OP_DIV_ASSIGN,
-    OP_MOD_ASSIGN,
-    OP_LSHIFT,
-    OP_RSHIFT,
-    OP_AND,
-    OP_OR,
-    OP_XOR,
-    OP_NOT,
-    OP_EQUAL,
-    OP_NOT_EQUAL,
-    OP_LESS_THAN,
-    OP_GREATER_THAN,
-    OP_LESS_THAN_EQUAL,
-    OP_GREATER_THAN_EQUAL,
-    OP_LOGICAL_AND,
-    OP_LOGICAL_OR,
-    OP_LOGICAL_NOT,
-    OP_BITWISE_AND,
-    OP_BITWISE_OR,
-    OP_BITWISE_XOR,
-    OP_BITWISE_NOT,
+        /* Punctuation */
+        case SEMICOLON:             return "SEMICOLON";
+        case COMMA:                 return "COMMA";
+        case OPEN_PARENTHESIS:      return "OPEN_PARENTHESIS";
+        case CLOSE_PARENTHESIS:     return "CLOSE_PARENTHESIS";
+        case OPEN_BRACKET:          return "OPEN_BRACKET";
+        case CLOSE_BRACKET:         return "CLOSE_BRACKET";
+        case OPEN_CURLY_BRACKET:    return "OPEN_CURLY_BRACKET";
+        case CLOSE_CURLY_BRACKET:   return "CLOSE_CURLY_BRACKET";
+        case COLON:                 return "COLON";
+        case POINTER_ARROW:         return "POINTER_ARROW";
+        case DOT:                   return "DOT";
+        case REFERENCE_AMPERSAND:   return "REFERENCE_AMPERSAND";
+        case POINTER_STAR:          return "POINTER_STAR";
+        case QUESTION_MARK:         return "QUESTION_MARK";
 
-    // Punctuation and Delimiters
-    SEMICOLON,
-    COMMA,
-    OPEN_PARENTHESIS,
-    CLOSE_PARENTHESIS,
-    OPEN_BRACKET,
-    CLOSE_BRACKET,
-    OPEN_CURLY_BRACKET,
-    CLOSE_CURLY_BRACKET,
-    COLON,
-    POINTER_ARROW,
-    DOT,
-    REFERENCE_AMPERSAND,
-    POINTER_STAR,
-    QUESTION_MARK,
+        /* Preprocessor / comments / whitespace / errors */
+        case PREPROCESSOR_DIRECTIVE: return "PREPROCESSOR_DIRECTIVE";
+        case SINGLE_LINE_COMMENT:    return "SINGLE_LINE_COMMENT";
+        case MULTI_LINE_COMMENT:     return "MULTI_LINE_COMMENT";
+        case NEWLINE:                return "NEWLINE";
+        case WHITESPACE:             return "WHITESPACE";
+        case ERROR_TOKEN:            return "ERROR_TOKEN";
+        case UNKNOWN:                return "UNKNOWN";
+        case END_OF_FILE:            return "END_OF_FILE";
 
-    // Preprocessor and Comments
-    PREPROCESSOR_DIRECTIVE,
-    SINGLE_LINE_COMMENT,
-    MULTI_LINE_COMMENT,
-    NEWLINE,
-    WHITESPACE,
-    
-    // Special tokens
-    ERROR_TOKEN,
-    UNKNOWN,
-    END_OF_FILE
-};
-
-// Map from enum values to human-readable strings {index corresponds to the enum value}
-const char* const token_names[] = {
-    "KEYWORD_INT",
-    "KEYWORD_CHAR",
-    "KEYWORD_FLOAT",
-    "KEYWORD_DOUBLE",
-    "KEYWORD_VOID",
-    "KEYWORD_IF",
-    "KEYWORD_ELSE",
-    "KEYWORD_FOR",
-    "KEYWORD_WHILE",
-    "KEYWORD_RETURN",
-    "KEYWORD_BREAK",
-    "KEYWORD_CONTINUE",
-    "KEYWORD_DO",
-    "KEYWORD_SWITCH",
-    "KEYWORD_CASE",
-    "KEYWORD_DEFAULT",
-    "KEYWORD_GOTO",
-    "KEYWORD_ENUM",
-    "KEYWORD_STRUCT",
-    "KEYWORD_UNION",
-    "KEYWORD_TYPEDEF",
-    "KEYWORD_STATIC",
-    "KEYWORD_AUTO",
-    "KEYWORD_CONST",
-    "KEYWORD_SIZEOF",
-    "KEYWORD_CLASS",
-    "KEYWORD_PUBLIC",
-    "KEYWORD_PRIVATE",
-    "KEYWORD_PROTECTED",
-    "KEYWORD_NEW",
-    "KEYWORD_DELETE",
-    "KEYWORD_USING",
-    "KEYWORD_NAMESPACE",
-    "KEYWORD_BOOL",
-    "KEYWORD_LONG",
-    "KEYWORD_TRUE",
-    "KEYWORD_FALSE",
-    "IDENTIFIER",
-    "INTEGER_LITERAL",
-    "FLOAT_LITERAL",
-    "CHAR_LITERAL",
-    "STRING_LITERAL",
-    "OP_PLUS",
-    "OP_MINUS",
-    "OP_MULTIPLY",
-    "OP_DIVIDE",
-    "OP_MODULO",
-    "OP_INCREMENT",
-    "OP_DECREMENT",
-    "OP_ASSIGN",
-    "OP_ADD_ASSIGN",
-    "OP_SUB_ASSIGN",
-    "OP_MUL_ASSIGN",
-    "OP_DIV_ASSIGN",
-    "OP_MOD_ASSIGN",
-    "OP_LSHIFT",
-    "OP_RSHIFT",
-    "OP_AND",
-    "OP_OR",
-    "OP_XOR",
-    "OP_NOT",
-    "OP_EQUAL",
-    "OP_NOT_EQUAL",
-    "OP_LESS_THAN",
-    "OP_GREATER_THAN",
-    "OP_LESS_THAN_EQUAL",
-    "OP_GREATER_THAN_EQUAL",
-    "OP_LOGICAL_AND",
-    "OP_LOGICAL_OR",
-    "OP_LOGICAL_NOT",
-    "OP_BITWISE_AND",
-    "OP_BITWISE_OR",
-    "OP_BITWISE_XOR",
-    "OP_BITWISE_NOT",
-    "SEMICOLON",
-    "COMMA",
-    "OPEN_PARENTHESIS",
-    "CLOSE_PARENTHESIS",
-    "OPEN_BRACKET",
-    "CLOSE_BRACKET",
-    "OPEN_CURLY_BRACKET",
-    "CLOSE_CURLY_BRACKET",
-    "COLON",
-    "POINTER_ARROW",
-    "DOT",
-    "REFERENCE_AMPERSAND",
-    "POINTER_STAR",
-    "QUESTION_MARK",
-    "PREPROCESSOR_DIRECTIVE",
-    "SINGLE_LINE_COMMENT",
-    "MULTI_LINE_COMMENT",
-    "NEWLINE",
-    "WHITESPACE",
-    "ERROR_TOKEN",
-    "UNKNOWN",
-    "END_OF_FILE"
-};
-
-// Function to convert token enum to string
-inline const char* token_to_string(TokenType token) {
-    if (token >= 1 && token <= END_OF_FILE) {
-        return token_names[token - 1];  // Adjust for 0-based array indexing
+        default:
+            return "UNKNOWN";
     }
-    return "UNKNOWN";
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
